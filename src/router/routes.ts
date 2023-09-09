da600522237b6da012db85606a08539e09d3c7b0
Fs-DXMY <fs.dxmy@gmail.com>
2023-09-09 20:20:23 +0800

    feat: 添加基础布局及登录页面

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,6 +8,35 @@ declare module 'vue-router' {
   }
 }
 
-const routes: RouteRecordRaw[] = []
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'nav',
+    redirect: '/index',
+    component: () => import('@/layouts/BasicLayout.vue'),
+    children: [
+      {
+        path: '/index',
+        meta: { title: '主页' },
+        component: () => import('@/views/IndexView.vue')
+      },
+      {
+        path: '/problem/all',
+        meta: { title: '题库' },
+        component: () => import('@/views/ProblemAllView.vue')
+      },
+      {
+        path: '/problem/manage',
+        meta: { title: '题目管理', requiredAuth: AuthEnum.USER },
+        component: () => import('@/views/ProblemManageView.vue')
+      }
+    ]
+  },
+  {
+    path: '/login',
+    meta: { title: '登录' },
+    component: () => import('@/views/LoginView.vue')
+  }
+]
 
 export default routes
---
+++ src/views/ProblemManageView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>题目管理</h1>
+</template>
