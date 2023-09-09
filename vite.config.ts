4bb344eda4341837d38f1c586814995580baffa4
Fs-DXMY <fs.dxmy@gmail.com>
2023-09-09 20:17:38 +0800

    Initial commit.

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 80
+  }
+})
