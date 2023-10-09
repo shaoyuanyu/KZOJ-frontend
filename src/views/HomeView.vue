82bae809ad093d25d1276095a92ff897bc5e0482
YSY <shaoyuanyu@foxmail.com>
2023-10-09 22:33:54 +0800

    style(主页):优化主页布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,31 +10,40 @@ import NewProblemCard from '@/components/home/NewProblemCard.vue'
   <div class="home-view">
     <a-row class="grid" :gutter="20">
       <a-col :span="12">
-        <div
-          style="
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-          "
-        >
-          <div style="width: 100%; height: 300px; margin-bottom: 10px"><WelcomeCard /></div>
-          <div style="width: 100%; height: 290px"><NoticeCard /></div>
+        <div style="flex-direction: column" class="column">
+          <div style="width: 100%; height: 300px; margin-bottom: 10px" class="home-cards">
+            <WelcomeCard />
+          </div>
+          <div style="width: 100%; height: 290px" class="home-cards">
+            <NoticeCard />
+          </div>
         </div>
       </a-col>
 
       <a-col :span="12">
-        <div style="width: 100%; height: 600px"><UserRankCard /></div>
+        <div class="column">
+          <div style="width: 100%; height: 600px" class="home-cards">
+            <UserRankCard />
+          </div>
+        </div>
       </a-col>
     </a-row>
 
     <a-row class="grid" :gutter="20">
       <a-col :span="12">
-        <div style="width: 100%; height: 400px"><StatisticsCard /></div>
+        <div class="column">
+          <div style="width: 100%; height: 400px" class="home-cards">
+            <StatisticsCard />
+          </div>
+        </div>
       </a-col>
 
       <a-col :span="12">
-        <div style="width: 100%; height: 400px"><NewProblemCard /></div>
+        <div class="column">
+          <div style="width: 100%; height: 400px" class="home-cards">
+            <NewProblemCard />
+          </div>
+        </div>
       </a-col>
     </a-row>
   </div>
@@ -47,8 +56,17 @@ import NewProblemCard from '@/components/home/NewProblemCard.vue'
 
 .grid .arco-col {
   margin-bottom: 10px;
-  /*line-height: 48px;*/
-  color: var(--color-white);
   text-align: center;
+}
+
+.column {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.home-cards {
+  width: 100%;
+  max-width: 1000px;
 }
 </style>
