ec74c5af3a8f8e196f59a1a4c4ad7f8c0cb5d000
Taiyu <222090218@stu.nau.edu.cn>
2023-10-09 14:00:43 +0800

    feat: 修改了关于界面

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,20 +1,41 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div style="width: 100%; height: 100%; display: flex; justify-content: center; align-items: center;">
-    <a-card hoverable :style="{ width: '560px' }">
+  <div class="container">
+    <a-card hoverable class="card">
       <template #cover>
-        <img src="/kz_logo.png" />
+        <div class="gradient-background">
+          <img src="/kz_logo.png" style="width: 200px; margin: 0 auto;" />
+        </div>
       </template>
-
-      <a-card-meta title="KZOJ">
+      <a-card-meta>
         <template #description>
-          <a href="https://github.com/shaoyuanyu/KZOJ-frontend">KZOJ</a> <br />
-          forked from
-          <a href="https://github.com/DXMY-Fs/MOJ-frontend">MOJ</a>
+          <div style="text-align: center;">
+            <a href="https://github.com/shaoyuanyu/KZOJ-frontend">KZOJ</a> 
+            <div>Forked From</div>
+            <a href="https://github.com/DXMY-Fs/MOJ-frontend">MOJ</a>
+            <div>团队功能开发贡献者 / （后期添加）</div>
+          </div>
         </template>
       </a-card-meta>
     </a-card>
-    
   </div>
 </template>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  width: 560px;
+}
+
+.gradient-background {
+  background: linear-gradient(to bottom, #87cfeb72, #f6ff0044);
+  text-align: center;
+  padding: 20px;
+}
+</style>
