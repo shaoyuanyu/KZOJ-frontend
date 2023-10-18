fe0215217694a7b8a84132fd89c99d843fd311d0
Taiyu <222090218@stu.nau.edu.cn>
2023-10-18 11:11:09 +0800

    style(训练页面):以树结构实现列表，实现折叠

--- src/components/train/ProblemListCard.vue
+++ src/components/train/ProblemListCard.vue
@@ -1,73 +1,127 @@
 <template>
   <div>
     <a-card :style="{ width: '100%' }" title="题目列表">
-      <ul>
-        <li v-for="(part, partIndex) in data" :key="partIndex">
-          <h2>{{ part.title }}</h2>
-          <ul>
-            <li v-for="(chapter, chapterIndex) in part.chapters" :key="chapterIndex">
-              <h3>{{ chapter.title }}</h3>
-              <ul>
-                <li v-for="(section, sectionIndex) in chapter.sections" :key="sectionIndex">
-                  <a-button :type="section.buttonType" class="left-align" long>
-                    <span>{{ section }}</span>
-                  </a-button>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
+      <a-tree
+        :data="treeData"
+        :default-expanded-keys='all'
+        :default-selected-keys="[]"
+        :size='large'
+        class="custom-tree"
+      />
     </a-card>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
 
 export default {
+  //示例数据
   setup() {
-    // 示例数据，多级列表
-    const data = reactive([
+
+    const treeData = [
       {
         title: '基础篇',
-        chapters: [
+        key: '0-0',
+        children: [
           {
             title: '第一部分 C++语言',
-            sections: ['C++语言入门', '顺序结构程序设计']
+            key: '0-0-0',
+            children: [
+              {
+                title: 'C++语言入门',
+                style: {
+                  backgroundColor: "#3491FA", 
+                }
+              },
+              {
+                title: '顺序结构程序设计',
+                style: {
+                  backgroundColor: "#3491FA", 
+                }
+              }
+            ]
           },
           {
             title: '第二部分 基础算法',
-            sections: ['高精度计算', '数据排序']
-          }
-        ]
+            key: '0-0-1',
+            children: [
+              {
+                title: '高精度计算',
+                style: {
+                  backgroundColor: "#3491FA",
+                }
+              },
+              {
+                title: '数据排序',
+                style: {
+                  backgroundColor: "#3491FA",
+                }
+              }
+            ]
+          },
+        ],
       },
       {
         title: '高级篇',
-        chapters: [
-          {
-            title: '第三部分 数据结构',
-            sections: ['数组', '链表', '树']
-          },
-          {
-            title: '第四部分 算法',
-            sections: ['排序算法', '查找算法']
-          }
-        ]
-      }
-    ])
+        key: '0-1',
+          children: [
+            {
+              title: '第三部分 数据结构',
+              key: '0-0-2',
+              children: [
+                {
+                  title: '数组',
+                  style: {
+                    backgroundColor: "#3491FA",
+                  }
+                },
+                {
+                  title: '链表',
+                  style: {
+                    backgroundColor: "#3491FA",
+                  }
+                }
+              ]
+            },
+            {
+              title: '第四部分 算法',
+              key: '0-0-3',
+              children: [
+                {
+                  title: '排序算法',
+                  style: {
+                    backgroundColor: "#3491FA", 
+                  }
+                },
+                {
+                  title: '查找算法',
+                  style: {
+                    backgroundColor: "#3491FA", // 设置背景颜色
+                  }
+                }
+              ]
+            },
+          ],
+      },
+    ];
+
+    const size = ref('large');
 
     return {
-      data
+      treeData,
+      size,
     }
   }
 }
 </script>
 
 <style scoped>
-.left-align {
-  text-align: left;
+.custom-tree .ant-tree-treenode.level-2 {
+  /* 第三级元素样式 */
+  background-color: blue; /* 背景颜色 */
+  border: 1px solid red; /* 边框属性 */
 }
-
 </style>
